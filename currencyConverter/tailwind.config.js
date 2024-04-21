ecb15438dbe0d893753326ff95873eac5df45b18
SohSingh <sohansingh3878@gmail.com>
2024-04-22 01:07:17 +0530

    react snippet

---
+++ currencyConverter/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  
+  theme: {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    extend: {},
+  },
+  plugins: [],
+}
+
